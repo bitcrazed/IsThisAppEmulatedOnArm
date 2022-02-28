7e6d398f8325db702efb8f9d33c434cad3e6df83
Rich Turner <rich@bitcrazed.com>
2022-02-28 13:02:43 -0800

    Added additional processor arch determination

--- IsThisAppEmulatedOnArm/main.cpp
+++ IsThisAppEmulatedOnArm/main.cpp
@@ -8,6 +8,16 @@ using namespace winrt;
 using namespace Windows::Foundation;
 using namespace std;
 
+const char* GetProcessorTypeString(UINT nProcArch)
+{
+	// Note: Non-exhaustive list of architectures, abbreviated for demo purposes.
+	return
+		(nProcArch == PROCESSOR_ARCHITECTURE_AMD64) ? "AMD64"
+		: (nProcArch == PROCESSOR_ARCHITECTURE_IA32_ON_ARM64) ? "x86 on ARM64"
+		: (nProcArch == PROCESSOR_ARCHITECTURE_ARM64) ? "ARM64"
+		: "Unsupported platform";
+}
+
 int main()
 {
 	init_apartment();
@@ -20,12 +30,19 @@ int main()
 		&nImageFileMachine, // Returns the type of WoW process, or IMAGE_FILE_MACHINE_UNKNOWN (0) if the target process is not a WOW64 process
 		&nNativeMachine))   // Returns IMAGE_FILE_MACHINE_* value identifying the native architecture of host system
 	{
+		PROCESS_MACHINE_INFORMATION pmi{};
+		BOOL gpiResult = GetProcessInformation(hCurrentProcess, ProcessMachineTypeInfo, &pmi, sizeof(pmi));
+
+		SYSTEM_INFO sysInfo{};
+		GetSystemInfo(&sysInfo);
+
 		const char* szProcessType
 		{
+			// Note: Non-exhaustive list of machine types, abbreviated for demo purposes.
 			(nImageFileMachine == IMAGE_FILE_MACHINE_AMD64) ? "x64"
 				: (nImageFileMachine == IMAGE_FILE_MACHINE_ARM64) ? "ARM64"
 				: (nImageFileMachine == IMAGE_FILE_MACHINE_I386) ? "X86"
-				: (nImageFileMachine == IMAGE_FILE_MACHINE_UNKNOWN) ? "Non-WOW64"
+				: (nImageFileMachine == IMAGE_FILE_MACHINE_UNKNOWN) ? GetProcessorTypeString(sysInfo.wProcessorArchitecture)
 				: "Unknown"
 		};
 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# IsThisAppEmulatedOnArm
+﻿# IsThisAppEmulatedOnArm
 Determines whether this x64 app is running emulated on an ARM64 device
+
+# ⚠️ Important Note: BE WARNED!
+This code is provided to illustrate one way to determine the architecture 
+of the current process when run on an ARM64 Windows PC. This code lacks 
+sufficient error checking and may require additional correctness checking 
+for use in a production app/system.
